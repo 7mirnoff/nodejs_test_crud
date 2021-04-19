c4fb10c92377f219c2821bd3b904390609e108b8
Smirnov Dmitrey <dima7mirnoff@gmail.com>
2021-04-19 12:00:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import expresss from 'express'
+import mongoose from 'mongoose'
+import fileUpload from 'express-fileupload'
+
+import router from './router.js'
+
+const PORT = 5000
+const DB_USER = 'mongodb+srv://user:user@cluster0.myssl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const app = expresss()
+
+app.use(expresss.json())
+app.use(expresss.static('static'))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+async function startApp () {
+  try {
+    await mongoose.connect(DB_USER, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+    app.listen(PORT, () => {
+      console.log('Server started on port: ' + PORT)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+startApp()
